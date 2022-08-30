5b748d0ab5555aad167b48710efedee4661a4fa3
amachea1 <108307052+amachea1@users.noreply.github.com>
2022-08-29 17:27:51 -0700

    HW website HTML

--- Algorithim Lesson 5/Lesson 5_HW.py
+++ Algorithim Lesson 5/Lesson 5_HW.py
@@ -13,6 +13,14 @@ print(selection_sort_desc([5,34,5,4646,242,2425252,2324,1]))
 
 #Bubble Sort Implement the bubble sort algorithm that is sorting in descending order.
 
+def bubble_sort_desc(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] < arr[j + 1]:
+                arr[j + 1], arr[j] = arr[j], arr[j + 1]
+    return arr
+print(bubble_sort_desc([3,5,42,443,4,2424,]))
+
 #Insertion Sort Implement the insertion sort algorithm that is sorting in descending order.
 def insertion_sort_desc(arr):
     for i in range(1, len(arr)):
